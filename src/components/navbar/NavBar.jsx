6b04075fbfbd17ee47d0bf6be4d215fa3f3fbb70
KMery <kmeryleiva@gmail.com>
2021-06-21 18:55:36 -0300

    Changing navbar style, adding skills

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -4,9 +4,9 @@ import { Navbar, Nav } from 'react-bootstrap';
 export const NavBar = () => {
     return (
         <div>
-            <Navbar bg="dark" variant="dark" fixed="top">
-                <Navbar.Brand href="#aboutMe">AboutMe</Navbar.Brand>
+            <Navbar bg="dark" variant="dark" fixed="top" className="d-flex justify-content-around">
                 <Nav className="mr-auto">
+                    <Nav.Link href="#aboutMe">AboutMe</Nav.Link>
                     <Nav.Link href="#skill">Skills</Nav.Link>
                     <Nav.Link href="#project">Projects</Nav.Link>
                     <Nav.Link href="#contact">Contact me</Nav.Link>
